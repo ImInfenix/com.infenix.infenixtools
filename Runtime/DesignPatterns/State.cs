ea95ee63254afb2af4d7ced88c9720cb9a038a49
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2021-05-20 20:08:36 +0200

    Implemented State abstract class to raise events on state changing

---
+++ Runtime/DesignPatterns/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InfenixTools.DesignPatterns
+{
+    public abstract class State<T, U> : Singleton<State<T, U>>, IState<U>
+    {
+        public event Action onEnter;
+        public event Action onExit;
+
+        public virtual void Enter(U objectInstance)
+        {
+            onEnter?.Invoke();
+        }
+
+        public abstract void Execute(U objectInstance);
+
+        public virtual void Exit(U objectInstance)
+        {
+            onExit?.Invoke();
+        }
+    }
+}
