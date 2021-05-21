6bc8c492d9c8670e1747b2237e6b9cf7a97ff334
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2021-05-21 15:50:54 +0200

    Added deactivable object

---
+++ Runtime/Misc/DeactivableObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace InfenixTools.Utils
+{
+    public class DeactivableObject : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject content;
+
+        public virtual void Enable()
+        {
+            if (content != null)
+                content.SetActive(true);
+        }
+
+        public virtual void Disable()
+        {
+            if (content != null)
+                content.SetActive(false);
+        }
+    }
+}
