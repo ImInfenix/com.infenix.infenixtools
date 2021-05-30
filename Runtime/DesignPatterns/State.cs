0abfe7dbeb620978c7acd56dd91f53ab5a205f58
ImInfenix <duncan.sourdin@pm.me>
2021-05-30 19:18:49 +0200

    Refactor generics type to be explicit

--- Runtime/DesignPatterns/State.cs
+++ Runtime/DesignPatterns/State.cs
@@ -2,23 +2,23 @@
 
 namespace InfenixTools.DesignPatterns
 {
-    public abstract class State<T, U> : Singleton<State<T, U>>, IState<U>
+    public abstract class State<SingletonType, StateType> : Singleton<State<SingletonType, StateType>>, IState<StateType>
     {
         public static event Action OnEnter;
         public static event Action OnExecute;
         public static event Action OnExit;
 
-        public virtual void Enter(U fre)
+        public virtual void Enter(StateType objectInstance)
         {
             OnEnter?.Invoke();
         }
 
-        public virtual void Execute(U objectInstance)
+        public virtual void Execute(StateType objectInstance)
         {
             OnExecute?.Invoke();
         }
 
-        public virtual void Exit(U objectInstance)
+        public virtual void Exit(StateType objectInstance)
         {
             OnExit?.Invoke();
         }
